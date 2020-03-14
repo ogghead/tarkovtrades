1b63e62437e726fe783f02446a656e06a2d32898
darwin <darwin@sprout.org>
2020-03-13 19:55:40 -0600

    Added true value, accurate fee calculation

--- TarkovTest/app/admin.py
+++ TarkovTest/app/admin.py
@@ -10,19 +10,19 @@ class InputInlineAdmin(admin.TabularInline):
 class OutputInlineAdmin(admin.TabularInline):
     model = Trade.output_items.through
 
-class ItemAddForm(ModelForm):
-    model = Item
-    fields = ('name', ('highest_sell_price_to_trader', 'highest_sell_price_trader'), 
-              ('lowest_buy_price_from_trader', 'lowest_buy_price_trader'), 
-              'market_buy_price')
+#class ItemAddForm(ModelForm):
+#    model = Item
+#    fields = ('name', 'true_value', ('highest_sell_price_to_trader', 'highest_sell_price_trader'), 
+#              ('lowest_buy_price_from_trader', 'lowest_buy_price_trader'), 
+#              'market_buy_price')
 
-class ItemChangeForm(ModelForm):
-    model = Item
-    fields = ('name', ('highest_sell_price_to_trader', 'highest_sell_price_trader'), 
-              ('lowest_buy_price_from_trader', 'lowest_buy_price_trader'),
-              'market_buy_price', 'market_sell_price_no_intel',
-              'market_sell_price_intel', 'min_buy_price',
-              'max_sell_price_no_intel', 'max_sell_price_intel')
+#class ItemChangeForm(ModelForm):
+#    model = Item
+#    fields = ('name', ('highest_sell_price_to_trader', 'highest_sell_price_trader'), 
+#              ('lowest_buy_price_from_trader', 'lowest_buy_price_trader'),
+#              'market_buy_price', 'market_sell_price_no_intel',
+#              'market_sell_price_intel', 'min_buy_price',
+#              'max_sell_price_no_intel', 'max_sell_price_intel')
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
@@ -35,14 +35,14 @@ class ItemAdmin(admin.ModelAdmin):
                        'max_sell_price_no_intel', 'max_sell_price_intel')
 
     def add_view(self, request, extra_content=None):
-         self.fields = ('name', ('highest_sell_price_to_trader', 'highest_sell_price_trader'), 
+         self.fields = ('name', 'true_value', ('highest_sell_price_to_trader', 'highest_sell_price_trader'), 
                         ('lowest_buy_price_from_trader', 'lowest_buy_price_trader'), 
                         'market_buy_price')
          self.readonly_fields = ()
          return super(ItemAdmin,self).add_view(request)
 
     def change_view(self, request, object_id, extra_content=None):
-         self.fields = ('name', ('highest_sell_price_to_trader', 'highest_sell_price_trader'), 
+         self.fields = ('name', 'true_value', ('highest_sell_price_to_trader', 'highest_sell_price_trader'), 
                         ('lowest_buy_price_from_trader', 'lowest_buy_price_trader'),
                         'market_buy_price', 'market_sell_price_no_intel',
                         'market_sell_price_intel', 'min_buy_price',
--- TarkovTest/app/models.py
+++ TarkovTest/app/models.py
@@ -4,25 +4,27 @@ import math
 from django.db import models
 from django.utils import timezone
 
-
 # Create your models here.
 class Item(models.Model):
-    traders = ['Prapor', 'Therapist', 'Fence', 'Skier',
+    traders = ['', 'Prapor', 'Therapist', 'Fence', 'Skier',
                'Peacekeeper', 'Mechanic', 'Ragman', 'Jaeger']
     traders = [(i, i) for i in traders]
     name = models.CharField(max_length=200)
+    true_value = models.IntegerField()
     highest_sell_price_to_trader = models.IntegerField() # Highest sell price of this item to a trader
     highest_sell_price_trader = models.CharField(choices=traders, max_length=200) # Trader for highest sell price
-    lowest_buy_price_from_trader = models.IntegerField(blank=True) # Lowest buy price of this item from a trader, may be blank if not available from traders
-    lowest_buy_price_trader = models.CharField(choices=traders, max_length=200, blank=True) # Trader for lowest buy price, may be blank if not available from traders
+    lowest_buy_price_from_trader = models.IntegerField(blank=True, null=True) # Lowest buy price of this item from a trader, may be blank if not available from traders
+    lowest_buy_price_trader = models.CharField(choices=traders, max_length=200, default=traders[0], blank=True) # Trader for lowest buy price, may be blank if not available from traders
     market_buy_price = models.IntegerField() # The seen price on the market
 
     def calculate_fee(self, post_price, intel_center):
-        v0 = self.lowest_buy_price_from_trader
+        v0 = self.true_value
         vr = post_price
         ti = tr = 0.025
         p0 = math.log10(v0/vr)
-        pr = math.log10(v0/vr)
+        #if v0 < vr: p0=math.pow(math.log10(v0/vr), 1.08)
+        pr = math.log10(vr/v0)
+        #math.pow(math.log10(vr/v0), 1.08) if v0 >= vr else 
         fee = v0 * ti * math.pow(4, p0) + vr * tr * math.pow(4, pr)
         if intel_center:
             fee = .7*fee
@@ -38,7 +40,7 @@ class Item(models.Model):
 
     @property
     def min_buy_price(self):
-        ls = [i for i in [self.market_buy_price, self.lowest_buy_price_from_trader] if i is not False]
+        ls = [i for i in [self.market_buy_price, self.lowest_buy_price_from_trader] if i is not None]
         return min(ls)
 
     @property
@@ -68,6 +70,23 @@ class Trade(models.Model):
     trader_level = models.CharField(choices=levels, max_length=200)
     input_items = models.ManyToManyField(Item, through='InputCount', related_name='inputs')
     output_items = models.ManyToManyField(Item, through='OutputCount', related_name='outputs')
+
+    @property
+    def inputs_sell_price_no_intel(self):
+        return sum([i.amount * i.item.max_sell_price_no_intel for i in self.input_items.all()])
+
+    @property
+    def inputs_sell_price_intel(self):
+        return sum([i.amount * i.item.max_sell_price_intel for i in self.input_items.all()])
+
+    @property
+    def outputs_sell_price_no_intel(self):
+        return sum([i.amount * i.item.max_sell_price_no_intel for i in self.output_items.all()])
+
+    @property
+    def outputs_sell_price_intel(self):
+        return sum([i.amount * i.item.max_sell_price_intel for i in self.output_items.all()])
+
 
 class InputCount(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
