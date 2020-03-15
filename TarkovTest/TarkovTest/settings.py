9badd282d9a2e776f1349a98b06d0af970e5eaf9
darwin <darwin@sprout.org>
2020-03-14 19:12:49 -0600

    Added SQL Server backend

--- TarkovTest/TarkovTest/settings.py
+++ TarkovTest/TarkovTest/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'TarkovTest.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'Tarkov',
+        'HOST': 'OGGSTATION',
+        'PORT': '',
+        'USER': '',
+        'PASSWORD': '',
+        'OPTIONS': {
+            'driver': "SQL Server Native Client 11.0",
+        },
     }
 }
 
