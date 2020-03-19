74405a085c92dcd90f3160f5d4031a1ecca0e03d
darwin <darwin@sprout.org>
2020-03-18 20:11:35 -0600

    Working item submission form, currently not hooked up

--- TarkovTrades/app/forms.py
+++ TarkovTrades/app/forms.py
@@ -5,6 +5,7 @@ Definition of forms.
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
+from app.models import Item
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
@@ -16,3 +17,9 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
+
+class ItemForm(forms.Form):
+    items = Item.objects.order_by('name')
+    item_names = [(i.name, i.name) for i in items]
+    item = forms.ChoiceField(choices=item_names)
+    item_amount = forms.IntegerField(min_value=1, max_value=100)
